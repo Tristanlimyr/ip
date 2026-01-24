744f7e39efa55d3c9a90f7f871afc237d335bc06
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-01-24 13:48:20 +0800

    Rename chatbot and add greeting

---
+++ src/main/java/Wout.java
@@ -0,0 +1,11 @@
+public class Wout {
+    public static void main(String[] args) {
+        String logo = "________________________________\n"
+                + "Hello! I'm Wout!\n"
+                + "What can I do for you?\n"
+                + "________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "________________________________";
+        System.out.println(logo);
+    }
+}
