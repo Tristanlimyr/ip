f49d972f5951724313c3ea1f77c4a6ce5cfeeaf1
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-01-24 14:48:55 +0800

    Use a class to represent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        // mark done task with X
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+}
