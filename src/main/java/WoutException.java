b420235f578ca808987f228a62fd2e4772371fdb
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-01-24 19:24:54 +0800

    Add WoutException class

---
+++ src/main/java/WoutException.java
@@ -0,0 +1,12 @@
+public class WoutException extends Exception {
+    protected String description;
+
+    public WoutException(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
