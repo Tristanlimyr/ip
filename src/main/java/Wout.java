cffe895c823df9d9a22cc72c61a0771e83de04f1
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-01-24 14:11:39 +0800

    Add support for echo

--- src/main/java/Wout.java
+++ src/main/java/Wout.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 public class Wout {
+    public static Scanner userCommandsScanner = new Scanner(System.in);
+    public static String exitCommand = "bye";
+    public static String greetingMessage =
+            "________________________________\n"
+            + "Hello! I'm Wout!\n"
+            + "What can I do for you?\n"
+            + "________________________________\n";
+
+    public static String exitMessage =
+            "________________________________\n"
+            + "Bye. Hope to see you again soon!\n"
+            + "________________________________";
+
     public static void main(String[] args) {
-        String logo = "________________________________\n"
-                + "Hello! I'm Wout!\n"
-                + "What can I do for you?\n"
-                + "________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "________________________________";
-        System.out.println(logo);
+        System.out.println(Wout.greetingMessage);
+
+        boolean exit = false;
+        while (!exit) {
+            // Read user command
+            String command = Wout.userCommandsScanner.nextLine();
+            if (command.equals(Wout.exitCommand)) {
+                exit = true;
+            } else {
+                // Echo user command
+                System.out.println("________________________________\n"
+                        + command
+                        + "\n________________________________");
+            }
+        }
+
+        System.out.println(Wout.exitMessage);
     }
 }
