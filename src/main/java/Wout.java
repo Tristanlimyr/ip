ae67029d873c77a3c6c4b210a6629f58c4219d46
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-01-24 14:41:41 +0800

    Add support for list

--- src/main/java/Wout.java
+++ src/main/java/Wout.java
@@ -3,34 +3,39 @@ import java.util.Scanner;
 public class Wout {
     public static Scanner userCommandsScanner = new Scanner(System.in);
     public static String exitCommand = "bye";
-    public static String greetingMessage =
-            "________________________________\n"
-            + "Hello! I'm Wout!\n"
-            + "What can I do for you?\n"
-            + "________________________________\n";
+    public static String listCommand = "list";
+    public static String greetingMessage = "Hello! I'm Wout!\n"
+            + "What can I do for you?\n";
+    public static String exitMessage = "Bye. Hope to see you again soon!\n";
 
-    public static String exitMessage =
-            "________________________________\n"
-            + "Bye. Hope to see you again soon!\n"
-            + "________________________________";
+    public static void printMessage(String message) {
+        System.out.println("________________________________\n"
+                + message
+                + "________________________________\n"
+        );
+    }
 
     public static void main(String[] args) {
-        System.out.println(Wout.greetingMessage);
+        Wout.printMessage(Wout.greetingMessage);
 
+        UserCommandsStore userCommandsStore = new UserCommandsStore();
         boolean exit = false;
         while (!exit) {
             // Read user command
             String command = Wout.userCommandsScanner.nextLine();
             if (command.equals(Wout.exitCommand)) {
                 exit = true;
+            } else if (command.equals(Wout.listCommand)) {
+                // List all user commands
+                Wout.printMessage(userCommandsStore.listCommands());
             } else {
+                // Store user command
+                userCommandsStore.storeCommand(command);
                 // Echo user command
-                System.out.println("________________________________\n"
-                        + command
-                        + "\n________________________________");
+                Wout.printMessage("added: " + command + "\n");
             }
         }
 
-        System.out.println(Wout.exitMessage);
+        Wout.printMessage(Wout.exitMessage);
     }
 }
