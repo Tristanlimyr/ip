da63f92931d08000c213141433bce01b1b690439
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-01-31 17:45:34 +0800

    Edit code to comply with coding standard

--- src/main/java/wout/task/TaskList.java
+++ src/main/java/wout/task/TaskList.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class TaskList {
 
-    private final List<Task> listOfTasks;
+    private final List<Task> tasks;
 
     public TaskList(List<Task> tasks) {
-        this.listOfTasks = tasks;
+        this.tasks = tasks;
     }
 
     public TaskList() {
@@ -16,36 +16,36 @@ public class TaskList {
     }
 
     public void storeTask(Task task) {
-        listOfTasks.add(task);
+        tasks.add(task);
     }
 
     public int getNumOfTasks() {
-        return listOfTasks.size();
+        return tasks.size();
     }
 
     public Task markTaskAt(int index) {
-        Task task = listOfTasks.get(index - 1);
+        Task task = tasks.get(index - 1);
         task.markAsDone();
         return task;
     }
 
     public Task unmarkTaskAt(int index) {
-        Task task = listOfTasks.get(index - 1);
+        Task task = tasks.get(index - 1);
         task.unmarkAsDone();
         return task;
     }
 
     public Task deleteTaskAt(int index) {
-        return listOfTasks.remove(index - 1);
+        return tasks.remove(index - 1);
     }
 
     public String listTasks() {
         String tasks = "Here are the tasks in your list:\n";
         int index = 1;
-        if (listOfTasks.isEmpty()) {
+        if (this.tasks.isEmpty()) {
             return "list is currently empty!\n";
         } else {
-            for (Task task : listOfTasks) {
+            for (Task task : this.tasks) {
                 tasks += index + ". " + task + "\n";
                 index++;
             }
@@ -54,7 +54,7 @@ public class TaskList {
     }
 
     public List<Task> getTasks() {
-        return listOfTasks;
+        return tasks;
     }
 
 }
