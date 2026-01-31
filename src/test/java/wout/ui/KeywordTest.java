a50c2da20e74f5809f29cd9ccce703377a65a761
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-01-31 15:38:58 +0800

    Add JUnit tests for Parser, Storage, Keyword

--- src/test/java/wout/ui/KeywordTest.java
+++ src/test/java/wout/ui/KeywordTest.java
@@ -1,4 +1,43 @@
 package wout.ui;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class KeywordTest {
+
+    @Test
+    public void fromString_invalidKeyword() {
+        try {
+            Keyword.fromString("Mark");
+            fail();
+        } catch (WoutException e) {
+            assertEquals(Ui.INVALID_COMMAND, e.toString());
+        }
+        try {
+            Keyword.fromString("Unmark");
+            fail();
+        } catch (WoutException e) {
+            assertEquals(Ui.INVALID_COMMAND, e.toString());
+        }
+        try {
+            Keyword.fromString("Todo");
+            fail();
+        } catch (WoutException e) {
+            assertEquals(Ui.INVALID_COMMAND, e.toString());
+        }
+        try {
+            Keyword.fromString("Deadline");
+            fail();
+        } catch (WoutException e) {
+            assertEquals(Ui.INVALID_COMMAND, e.toString());
+        }
+        try {
+            Keyword.fromString("Event");
+            fail();
+        } catch (WoutException e) {
+            assertEquals(Ui.INVALID_COMMAND, e.toString());
+        }
+    }
 }
--- src/test/java/wout/ui/StorageTest.java
+++ src/test/java/wout/ui/StorageTest.java
@@ -1,13 +1,21 @@
 package wout.ui;
 
 import org.junit.jupiter.api.Test;
+import wout.task.Task;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class Storage {
+public class StorageTest {
     @Test
     public void load_fileNotFound() {
-        assertEquals(true, new Storage("file.txt").load());
+        try {
+            List< Task> tasks = new Storage("file.txt").load();
+            assertEquals(true, tasks.isEmpty());
+        } catch (WoutException e) {
+            fail();
+        }
     }
 }
